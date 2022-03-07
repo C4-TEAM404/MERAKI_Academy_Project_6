d21ae5f820465636740893599b8aad76b5f82e55
HaithamNawwaf <haithamalulaimi@gmail.com>
2022-03-07 14:15:59 +0200

    update student by id & install front end

--- backend/Routes/Student.js
+++ backend/Routes/Student.js
@@ -8,12 +8,16 @@ const {
   GetAllStudents,
   GetStudentById,
   DeleteStudentById,
+  UpdateStudentById,
 } = require("../controllers/Student");
+
+const { authentication } = require("../Middleware/authentication");
 
 studentRouter.post("/new", CreateNewStudent);
 studentRouter.get("/getall", GetAllStudents);
 studentRouter.get("/getbyid", GetStudentById);
 studentRouter.delete("/deletebyid/:id", DeleteStudentById);
+studentRouter.put("/updatebyid", authentication, UpdateStudentById);
 
 //====================================================// module.exports
 module.exports = studentRouter;
