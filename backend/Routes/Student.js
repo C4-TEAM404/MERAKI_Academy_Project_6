dbe2c224842203e20f52898dea505a8126cbb8cb
HaithamNawwaf <haithamalulaimi@gmail.com>
2022-03-07 08:41:52 +0200

    Build Routers

---
+++ backend/Routes/Student.js
@@ -0,0 +1,8 @@
+//====================================================//Require
+const express = require("express");
+const studentRouter = express.Router();
+
+//====================================================//Require Functions
+
+//====================================================// module.exports
+module.exports = studentRouter;
