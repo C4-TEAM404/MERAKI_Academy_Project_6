92fdd60c265cd6cdef6c42a30c59127144fcbd61
HaithamNawwaf <haithamalulaimi@gmail.com>
2022-03-07 08:30:14 +0200

    Build Database and make a connection

---
+++ backend/Database/Schema.sql
@@ -0,0 +1,8 @@
+-- ====================================================//Drop DataBase
+Drop Database if exists HEX;
+
+-- ====================================================//Create DataBase
+CREATE Database HEX;
+
+-- ====================================================//Use DataBase
+Use HEX;
