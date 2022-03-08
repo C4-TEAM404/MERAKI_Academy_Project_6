8f8529a708577ebcfb907695d06416cc24a4b87f
OmarKataa <omar.mahmoud.kataa@gmail.com>
2022-03-08 09:27:34 +0300

    morning error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,28 +3,24 @@ import React, { useState, useEffect, createContext } from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Login from "./Component/Login/Login";
 import Register from "./Component/Register/Register";
+import Header from "./Component/Header/Header";
+import Footer from "./Component/Footer/Footer";
+import Introduction from "./Component/Introduction/Introduction";
 export const UserContext = createContext();
-import Header from "../src/Component/Header/Header";
-import Footer from "../src/Component/Footer/Footer";
-import Introduction from "../src/Component/Introduction/Introduction";
 function App() {
   const [login, setLogin] = useState({});
   return (
     <div className="App">
-     <Header />
+      <Header />
       <UserContext.Provider value={{ login, setLogin }}>
         <Routes>
           <Route path="/" />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} 
-    
-     
-     
-        <Route path="/" element={<Introduction />} />
-     
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Introduction />} />
+        </Routes>
+      </UserContext.Provider>
       <Footer />
-           </Routes>
-           </UserContext.Provider>
     </div>
   );
 }
