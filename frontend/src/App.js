99146878f8c62d0dfd5610310a5891fea76b861c
OmarKataa <omar.mahmoud.kataa@gmail.com>
2022-03-10 00:39:17 +0300

    create course front end with video

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,23 @@ import Header from "./Component/Header/Header";
 import Footer from "./Component/Footer/Footer";
 import Introduction from "./Component/Introduction/Introduction";
 import Register_Teacher from "./Component/Register_Teacher/Register_Teacher";
-
+import Course_Details from "./Component/Course_Details/Course_Details";
+import Create_Course from "./Component/Create_Course/Create_Course";
 
 function App() {
   const [login, setLogin] = useState({});
+  const [courseId, setCourseId] = useState({});
   return (
     <div className="App">
       <Header />
-      <UserContext.Provider value={{ login, setLogin }}>
+      <UserContext.Provider value={{ login, setLogin, courseId, setCourseId }}>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register_Student />} />
           <Route path="/registerteacher" element={<Register_Teacher />} />
           <Route path="/" element={<Introduction />} />
+          <Route path="/coursedescryption" element={<Course_Details />} />
+          <Route path="/createcourse" element={<Create_Course />} />
         </Routes>
         <Footer />
       </UserContext.Provider>
