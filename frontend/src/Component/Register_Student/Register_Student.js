f02b5b8db07925e879f50a8b1ba0a49e36b58f0c
HaithamNawwaf <haithamalulaimi@gmail.com>
2022-03-11 21:59:50 +0200

    fix register student form

--- frontend/src/Component/Register_Student/Register_Student.js
+++ frontend/src/Component/Register_Student/Register_Student.js
@@ -61,7 +61,6 @@ const Register_Student = () => {
 
   const submit_Handler = async (e) => {
     e.preventDefault();
-
     try {
       const res = await axios.post("http://localhost:5000/student/new", {
         firstName,
@@ -72,8 +71,7 @@ const Register_Student = () => {
         profileImage,
         roleId: 3,
       });
-
-      console.log(res);
+      console.log(res, "-------------------");
       setFirstName("");
       setLastName("");
       setPhone("");
@@ -90,6 +88,143 @@ const Register_Student = () => {
   return (
     <div className="register_student-pageDiv">
       <div className="register_student-mainDiv">
+        <section class=" border-0">
+          <div class="container h-100 border-0">
+            <div class="row d-flex justify-content-center align-items-center h-100 border-0">
+              <div class="col-lg-12 col-xl-11 border-0">
+                <div class="card text-black border-0">
+                  <div class="card-body p-md-5 border-0">
+                    <div class="row justify-content-center border-0">
+                      <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1 border-0">
+                        <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 border-0">
+                          Sign up
+                        </p>
+
+                        <form class="mx-1 mx-md-4" onSubmit={submit_Handler}>
+                          <div class="d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                            <div class="form-outline flex-fill mb-0">
+                              <input
+                                type="text"
+                                id="form3Example1c"
+                                class="form-control"
+                                required
+                                onChange={firstName_handler}
+                                value={firstName}
+                              />
+                              <label class="form-label" for="form3Example1c">
+                                First Name{" "}
+                              </label>
+                            </div>
+                          </div>
+                          <div class="d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                            <div class="form-outline flex-fill mb-0">
+                              <input
+                                type="text"
+                                id="form3Example1c"
+                                class="form-control"
+                                required
+                                onChange={lastName_handler}
+                                value={lastName}
+                              />
+                              <label class="form-label" for="form3Example1c">
+                                Last Name
+                              </label>
+                            </div>
+                          </div>
+                          <div class="d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                            <div class="form-outline flex-fill mb-0">
+                              <input
+                                type="text"
+                                id="form3Example1c"
+                                class="form-control"
+                                required
+                                onChange={phone_handler}
+                                value={phone}
+                              />
+                              <label class="form-label" for="form3Example1c">
+                                Phone{" "}
+                              </label>
+                            </div>
+                          </div>
+
+                          <div class="d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                            <div class="form-outline flex-fill mb-0">
+                              <input
+                                type="email"
+                                id="form3Example3c"
+                                class="form-control"
+                                required
+                                onChange={email_handler}
+                                value={email}
+                              />
+                              <label class="form-label" for="form3Example3c">
+                                Your Email
+                              </label>
+                            </div>
+                          </div>
+
+                          <div class="d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                            <div class="form-outline flex-fill mb-0">
+                              <input
+                                type="password"
+                                id="form3Example4c"
+                                class="form-control"
+                                required
+                                onChange={password_handler}
+                                value={password}
+                              />
+                              <label class="form-label" for="form3Example4c">
+                                Password
+                              </label>
+                            </div>
+                          </div>
+                          <div class="d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                            <div class="form-outline flex-fill mb-0">
+                              <input
+                                type="file"
+                                class="form-control"
+                                id="customFile"
+                                onChange={loading}
+                              />
+
+                              <label class="form-label" for="form3Example4c">
+                                Upload Profile Image
+                              </label>
+                            </div>
+                          </div>
+
+                          <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                            <button
+                              type="submit"
+                              class="btn btn-primary btn-lg"
+                            >
+                              Register
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                      <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                        <img
+                          src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                          class="img-fluid"
+                          alt="Sample image"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* 
+
         <div className="header">
           <h2 className="Register_header">Register</h2>
         </div>
@@ -164,16 +299,16 @@ const Register_Student = () => {
               <button className="submit_button">Register</button>
             </div>
           </form>
-        </div>
+        </div> */}
       </div>
 
-      <div className="show_Image">
+      {/* <div className="show_Image">
         <img
           src={profileImage}
           alt="Profile_Picture"
           className="show_Image_img"
         />
-      </div>
+      </div> */}
     </div>
   );
 };
