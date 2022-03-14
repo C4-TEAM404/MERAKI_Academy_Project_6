b4381756d67e44c29a0045e36a7d96055afa2774
OmarKataa <omar.mahmoud.kataa@gmail.com>
2022-03-14 23:47:11 +0300

    json packages

--- backend/Routes/Course.js
+++ backend/Routes/Course.js
@@ -12,6 +12,7 @@ const {
   DeleteCourseById,
   UpdateCourseById,
   update_room_video_code,
+  getUser_Course,
 } = require("../controllers/Course");
 
 courseRouter.post("/new", CreateNewCourse);
@@ -22,6 +23,7 @@ courseRouter.post("/getByT", GetCourseByTitle);
 courseRouter.delete("/deletebyid/:id", DeleteCourseById);
 courseRouter.put("/updatebyid/:id", UpdateCourseById);
 courseRouter.put("/updateroomid", update_room_video_code);
+courseRouter.post("/usercourse", getUser_Course);
 
 //====================================================// module.exports
 module.exports = courseRouter;
